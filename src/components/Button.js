9ed0122cd892d15e73d957953fa56ba1c4cf0d68
Shubham Singh <shubham88singh@gmail.com>
2020-08-03 23:09:13 +0530

    Adding the button component

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+const Button = ({ name }) => {
+  return <div></div>;
+};
+
+Button.protoTypes = {
+  name: PropTypes.string,
+};
+export default Button;
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,7 +1,11 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 const Display = ({ result = '0' }) => {
   return <div>{result}</div>;
+};
+
+Display.propTypes = {
+  result: PropTypes.string,
 };
 
 export default Display;

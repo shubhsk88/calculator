0dc6c9da7f1fac05f00ccb8dfa5c9d2d97296a5e
Shubham Singh <shubham88singh@gmail.com>
2020-08-04 21:27:28 +0530

    converting the app component into class based component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div className="App">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { total: null, next: null, operation: null };
+  }
+  handleClick = (buttonName) => {
+    this.setState(calculate(this.state, buttonName));
+  };
+
+  render() {
+    const { total, next, operation } = this.state;
+    return (
+      <div className="App">
+        <Display total={total} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <Display />
+//       <ButtonPanel />
+//     </div>
+//   );
+// }
 
 export default App;
